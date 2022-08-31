aa36196e804949ba936f478a4629e96ea8b37f5a
Stanislav Makarkin <s.makarkin@andersenlab.com>
2022-08-31 10:45:22 +0000

    Feat/contacts: create Contacts page (jsx, constants, layout); routing to Contacts page (link in header), template components for footer, telephone blocks; props in header

--- src/modules/template-page/header/constants.ts
+++ src/modules/template-page/header/constants.ts
@@ -12,3 +12,12 @@ export const englishLanguage = {
 
 export const LANGUAGE_BUTTON_RU_TEXT = 'Ru';
 export const LANGUAGE_BUTTON_EN_TEXT = 'En';
+
+export const englishLanguage = {
+  locationText: 'Bank branches & ATMs',
+  coursesText: 'Exchange rates',
+  contactsText: 'Contact',
+} as const;
+
+export const LANGUAGE_BUTTON_RU_TEXT = 'Ru';
+export const LANGUAGE_BUTTON_EN_TEXT = 'En';
--- src/modules/ui-kit/buttons/back-button/types.ts
+++ src/modules/ui-kit/buttons/back-button/types.ts
@@ -1,4 +1,4 @@
 export type Props = {
-  handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   text: string;
 };
