929b43eb280b7629c4b0923a51bd6cfc9e47c054
Zoia Achkasova <z.achkasova@andersenlab.com>
2022-09-12 19:21:16 +0000

    add load-page

---
+++ src/modules/loans-page/my-loans/my-loans-main/constants.ts
@@ -0,0 +1,21 @@
+export const loansInfo = [
+  {
+    index: '1.',
+    loanName: 'Medical Loan',
+    duration: '4 years (48 months)',
+    amount: '30,549.00',
+  },
+  {
+    index: '2.',
+    loanName: 'Personal Loan',
+    duration: '6 years (72 months)',
+    amount: '40,000.00',
+  },
+  {
+    index: '3.',
+    loanName: 'Student Loan',
+    duration: '12 years (144 months)',
+    amount: '100,000.00',
+  },
+] as const;
+
