643bd5133b45b945d0bb4d8adecac9139a1fcec7
Speede <Speedew0w@yandex.ru>
2022-09-20 12:55:02 +0300

    chore(*): change auth prop for QA team

--- src/index.tsx
+++ src/index.tsx
@@ -7,21 +7,21 @@ import { Header } from './modules/template-page/header';
 import './index.scss';
 import { useState } from 'react';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 let isAuth = true;
-const logout = () => isAuth = false;
+const logout = () => (isAuth = false);
 //let [isAuth, setIsAuth] = useState(true);
 //const logout = () => setIsAuth(isAuth = false)
-export const AuthContext = React.createContext({ isAuth: true, fn: () => { } });
+export const AuthContext = React.createContext({ isAuth: true, fn: () => {} });
 
 root.render(
   <BrowserRouter>
     <AuthContext.Provider value={{ isAuth: isAuth, fn: logout }}>
       <Header />
-      {!isAuth && (<StartPage />)}
-      {isAuth && (<MainPage />)}
+      {!isAuth && <StartPage />}
+      {isAuth && <MainPage />}
     </AuthContext.Provider>
   </BrowserRouter>
 );
