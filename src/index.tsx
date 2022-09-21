1e211257b3a1af4794404c63e32662bb0d58fef4
ktihonov93 <k_tihonov@mail.ru>
2022-09-21 09:19:31 +0300

    fix(loan-products): remove comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
@@ -5,8 +6,16 @@ import { StartPage } from './modules/start-page';
 import { MainPage } from './modules/main-page';
 import { Header } from './modules/template-page/header';
 import './index.scss';
+=======
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { StartPage } from "./modules/start-page";
+import { MainPage } from "./modules/main-page";
+import "./index.scss";
+>>>>>>> c29f762 (fix(loan-products): remove comments)
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 let isAuth = true;
