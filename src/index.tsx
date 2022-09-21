c29f76292957b9e049f3525988a25a0dcb845c5b
ktihonov93 <k_tihonov@mail.ru>
2022-09-21 09:19:31 +0300

    fix(loan-products): remove comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { StartPage } from './modules/start-page';
-import { MainPage } from './modules/main-page';
-import './index.scss';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { StartPage } from "./modules/start-page";
+import { MainPage } from "./modules/main-page";
+import "./index.scss";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 const isAuth = true;
